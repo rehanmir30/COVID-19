33d69f64263920325638c3913c10d4eadc15f2e2
rehan <rehanmir3091997@gmail.com>
2020-04-11 13:32:33 +0500

    2 screens

---
+++ app/src/main/java/co/yactech/covid_19/SplashScreen.java
@@ -0,0 +1,37 @@
+package co.yactech.covid_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logo = findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+
+            }
+        }, 3000);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splashannimation);
+        logo.startAnimation(animation);
+
+    }
+}
